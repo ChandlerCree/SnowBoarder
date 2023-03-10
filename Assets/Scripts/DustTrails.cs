26647bb62c5fd7ad844dbc7e33c26e4d4aaacbb0
ChandlerCree <chandlercreegk@gmail.com>
2023-03-10 01:30:31 -0500

    First commit, Snowboarder Project

---
+++ Assets/Scripts/DustTrails.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DustTrails : MonoBehaviour
+{
+    [SerializeField] ParticleSystem trailEffect;
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.tag == "Ground")
+        {
+            trailEffect.Play();
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other) {
+        if(other.gameObject.tag == "Ground")
+        {
+            trailEffect.Stop();
+        }
+    }
+}
